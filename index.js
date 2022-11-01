b3d70ed26f1d6bab8db9a28a92b262bf1cb9aea1
teedaa <pengtedachengllhs@gmail.com>
2022-10-31 23:27:13 -0700

    Add function to link dayabase to sql

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password',
+    database: 'movie_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
